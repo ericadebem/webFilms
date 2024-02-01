d0a7564db5f33ba44fac196e1ed14d7a806bd05a
Erica de Bem <ericadebemm@gmail.com>
2024-02-01 14:48:19 +0100

    Add patchUser and deleteUser functions to userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,8 +1,15 @@
 import express from "express";
 
-import { postUser,getUser, getAllUser,  } from "../controller/userController.js";
+import {
+  postUser,
+  getUser,
+  getAllUser,
+  patchUser,
+  deleteUser,
+  deleteAllUsers,
+} from "../controller/userController.js";
 
 export const userRouter = express.Router();
 
-userRouter.route("/").post(postUser).get(getAllUser);
-userRouter.route("/:id").get(getUser)
+userRouter.route("/").post(postUser).get(getAllUser).delete(deleteAllUsers);
+userRouter.route("/:id").get(getUser).patch(patchUser).delete(deleteUser);
